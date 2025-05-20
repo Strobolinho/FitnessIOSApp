40200ab49665f5421180485c29c7802def9c366d
Nico Ströbel <nicolas.stroebel@icloud.com>
2025-05-20 03:54:47 +0200

    Erster Test

--- FitnessApp/ContentView.swift
+++ FitnessApp/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "dumbbell.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Fitness apppppp !!!")
         }
         .padding()
     }

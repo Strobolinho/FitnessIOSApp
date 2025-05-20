2c20fbec3e55842cb2977b8661868349db2c0240
Nico Ströbel <nicolas.stroebel@icloud.com>
2025-05-20 03:37:47 +0200

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Nicolas Ströbel on 20.05.25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
